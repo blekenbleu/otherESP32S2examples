309cc1d1e0f5498ed1f1253c6ca8848265c07333
blekenbleu <blekenbleu@gmail.com>
2021-11-21 14:53:11 -0500

    optionally trace RMT 24-word packets

--- ESP32WebRGBsliders/ESP32WebRGBsliders.ino
+++ ESP32WebRGBsliders/ESP32WebRGBsliders.ino
@@ -18,6 +18,47 @@ const char* Param_ID = "id";
 unsigned int count = 0;
 int r = 5, g = 2, b = 8;
 
+#define RMT_TRACE 0
+#if RMT_TRACE
+void testWS2812(u8 r, u8 g, u8 b)
+{
+  u32 color = r << 16 | g << 8 | b;
+  rmt_data_t led_data[24];
+  int bit;
+  static u32 old_color = 0;
+  if (old_color == color)
+    return;
+  old_color = color;
+  Serial.write("testWS2812(");
+  Serial.print(color, HEX);
+  Serial.write(");\n");
+
+  for (bit = 0; bit < 24; bit++) {
+    if (color & (1 << (23 - bit))) {
+      led_data[bit].level0 = 1;
+      led_data[bit].duration0 = 8;
+      led_data[bit].level1 = 0;
+      led_data[bit].duration1 = 4;
+    } else {
+      led_data[bit].level0 = 1;
+      led_data[bit].duration0 = 4;
+      led_data[bit].level1 = 0;
+      led_data[bit].duration1 = 8;
+    }
+    Serial.write("rmt_data_t led_data[");
+    Serial.print(bit);
+    Serial.write(") = ");
+    Serial.println(led_data[bit].val, HEX);
+  }
+  ESP32_LED(r,g,b);
+}
+
+#define TEST_LED(r,g,b) testWS2812(r,g,b);
+
+#else
+#define TEST_LED(r,g,b) ESP32_LED(r,g,b)
+#endif // RMT_TRACE
+
 // HTML: rename to range.htm to test in browsers
 #include "range.h"	// const char index_html[]
 
@@ -26,6 +67,8 @@ void notFound(AsyncWebServerRequest *request) {
 }
 
 void setup() {
+  ESP32_WS2812_SETUP(10);      // WS2812 red
+
   Serial.begin(115200);
   Serial.println("--- ESP32_WebRGB_ColorPicker Start---");
   Serial.print("Chip Model: ");
@@ -34,8 +77,6 @@ void setup() {
   Serial.println(ESP.getChipRevision());
   Serial.println();
   
-  ESP32_WS2812_SETUP(0);      // WS2812 all off
-
   //Act as STA, connect to WiFi network
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -45,7 +86,8 @@ void setup() {
   }
   Serial.write("\nVisit IP Address: ");
   Serial.println(WiFi.localIP());
-
+  ESP32_LED(0,10,0);
+  
   // Send HTML
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html);
@@ -115,11 +157,11 @@ void loop() {
   if(count > (10+r+g+b))
     count =  11;	// loop colors here
   if (count > (10+r+g))
-    ESP32_LED(0,0,count-(10+r+g));   // blue is dimmer
+    TEST_LED(0, 0, count-(10+r+g));   // blue is dimmer
   else if (count > 10+r)
-    ESP32_LED(0,count-(10+r),0);     // plenty bright
+    TEST_LED(0, count-(10+r), 0);     // plenty bright
   else if (count > 10)
-    ESP32_LED(count - 10,0, 0);
+    TEST_LED(count - 10, 0, 0);
   else delay(800);	     // hold web colors longer
   delay(300);
   count++;
